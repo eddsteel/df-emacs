d078c895e7f5cc2e1a46066ed224989ca2811603
Edd Steel <edward.steel@gmail.com>
2015-04-18 23:17:16 -0700

    Move some stuff to secrets for BNC support

--- edd/edd-erc.el
+++ edd/edd-erc.el
@@ -1,12 +1,10 @@
-(setq erc-port 6667)
 (setq erc-server-auto-reconnect nil)
-(setq erc-server "chat.freenode.net")
 
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
 ;; for autoload
 (defun edd-erc ()
   (interactive)
-  (edd-with-secrets "erc" (erc)))
+  (edd-with-secrets "erc" (erc :password erc-password)))
 
 (provide 'edd-erc)
