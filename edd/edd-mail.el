715fb00d220d823f9bac8e0168c2702fcb7a9d4e
Edd Steel <edward.steel@hootsuite.com>
2015-04-20 19:23:58 -0700

    clean up/fix notmuch

--- edd/edd-mail.el
+++ edd/edd-mail.el
@@ -17,23 +17,39 @@
                         message-send-mail-function 'message-send-mail-with-sendmail))
 
 ;; mail with notmuch
-(defun edd-notmuch-mark-search-read ()
-  (interactive)
-  (notmuch-search-tag-all '("-unread")))
-
 
 (require 'org-notmuch)
 
 (defun edd-mailbox ()
   (interactive)
   "Open default mailbox"
-  (nm))
+  (notmuch))
+
+(use-package notmuch
+  :ensure t
+  :config
+  (setq notmuch-saved-searches
+        '((:name "inbox" :query "tag:inbox" :key "i")
+          (:name "home-in" :query "folder:gmail/INBOX and tag:inbox")
+          (:name "unread" :query "tag:unread" :key "u")
+          (:name "flagged" :query "tag:flagged" :key "f")
+          (:name "sent" :query "tag:sent" :key "t")
+          (:name "drafts" :query "tag:draft" :key "d")
+          (:name "all mail" :query "*" :key "a")
+          (:name "scala-queue" :query "tag:scala-user and tag:unread")
+          (:name "akka-queue" :query "tag:akka-user and tag:unread")
+          (:name "arch-queue" :query "tag:arch-general and tag:unread")))
+  (defun edd-notmuch-mark-search-read ()
+    (interactive)
+    (notmuch-search-tag-all '("-unread")))
+  (bind-key "M" 'edd-notmuch-mark-search-read 'notmuch-search-mode-map));;))
+
 
 (use-package notmuch-unread
-  :ensure notmuch
   :ensure t
   :config
   (setq notmuch-unread-search-term "tag:unread and tag:inbox")
+
   (defun notmuch-unread-update-handler ()
     "Update the mode line."
     (require 'notmuch)
