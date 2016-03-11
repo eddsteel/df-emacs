b167d7a083771fe37fecbed1762a5c513b20e5c2
Edd Steel <edward.steel@hootsuite.com>
2016-03-10 20:59:08 -0800

    Apparently vandev went away

--- edd/edd-erc.el
+++ edd/edd-erc.el
@@ -6,14 +6,11 @@
   (setq edd-erc-to-shorten nil)
   (setq edd-erc-shortened nil)
   (mapc (lambda (n)
-          (cond ((string= "#vandev" n) (add-to-list 'edd-erc-shortened "#vd"))
-                ((string= "#vanops" n) (add-to-list 'edd-erc-shortened "#vo"))
+          (cond ((string= "#vandevops" n) (add-to-list 'edd-erc-shortened "#vd"))
                 (t (add-to-list 'edd-erc-to-shorten n)))) names)
   (append edd-erc-shortened (erc-track-shorten-names edd-erc-to-shorten)))
 
 (setq erc-track-shorten-function 'edd-erc-shorten-names)
-
-
 
 
 
