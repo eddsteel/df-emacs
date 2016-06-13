15292de6b72082956320b9e83fed99975dc41351
Edd Steel <edward.steel@gmail.com>
2016-06-12 19:52:44 -0700

    embrace racer

--- edd/edd-rust.el
+++ edd/edd-rust.el
@@ -14,4 +14,13 @@
     :init
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
+
+(use-package ac-racer
+  :init
+  (setq racer-cmd (expand-file-name "~/.cargo/bin/racer"))
+  (setq racer-rust-src-path (expand-file-name "~/src/rustc-1.9.0/src"))
+  (add-hook 'rust-mode-hook #'ac-racer-setup)
+  :commands (ac-racer-setup)
+  :ensure t)
+
 (provide 'edd-rust)
