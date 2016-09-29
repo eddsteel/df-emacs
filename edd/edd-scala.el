301f52154f0e7a13a99f7ad3c99c6f6168d296ff
Edd Steel <edward.steel@hootsuite.com>
2016-09-29 13:36:39 -0700

    some ensime config and helper functions
    helpers:
    
    - `edd-scala-sort-imports`: sort imports by rules
    - `edd-scala-ignore-style`: easy scalstyle ignore comments

--- edd/edd-scala.el
+++ edd/edd-scala.el
@@ -27,6 +27,8 @@
   (setq ensime-goto-test-config-defaults
         (plist-put ensime-goto-test-config-defaults
                    :test-template-fn 'edd-ensime-test-template))
+  (setq ensime-use-helm 't)
+  (setq ensime-graphical-tooltips 't)
   (local-set-key (kbd "C-c C-e") 'ensime-inf-eval-region)
   :commands
   (ensime-scala-mode-hook ensime-config-find-file ensime-connection-or-nil)
@@ -127,9 +129,8 @@ class %TESTCLASS% extends FlatSpec with Matchers {
   (call-interactively 'helm-occur "\bdef\ "))
 (add-hook 'scala-mode-hook (lambda () (local-set-key (kbd "C-c .") 'edd-scala-helm-method)))
 
-
 (setq edd-scala-sort-imports-rules
-      '(("^import com\\.hootsuite\\." . "1") ("^import scala\\." . "6") ("^import java\\." . "7") ("^import " . "5")))
+      '(("^import com\\.hootsuite\\." . "1") ("^import scala\\." . "6") ("^import java\\." . "7") ("^import javax\\." . "8") ("^import " . "5")))
 
 ;; TODO: support blank lines as both a rule and while traversing
 (defun edd-scala-sort-imports ()
@@ -137,34 +138,56 @@ class %TESTCLASS% extends FlatSpec with Matchers {
   (interactive)
   (save-excursion
     (beginning-of-buffer)
-    (search-forward-regexp "^package\\b")
-    (search-forward-regexp "^import\\b")
-    (beginning-of-line)
-    (let ((start (point)))
-      (while (looking-at-p "^import\\b")
-        (forward-line))
-      (end-of-line)
-      (let ((end (point)))
-        (goto-char start)
-        (while (looking-at-p "^import\\b")
+    (let ((importre "^import\\b")
+          (blankre  "^$"))
+      (search-forward-regexp "^package\\b")
+      (search-forward-regexp importre)
+      (beginning-of-line)
+      (let ((start (point)))
+        (while (looking-at-p importre)
+          (forward-line))
+        (end-of-line)
+        (let ((end (point)))
           (mapcar
            (lambda (pair)
-             (let ((rule (car pair))
-                   (ord (cdr pair)))
-               (when
-                   (search-forward-regexp rule (line-end-position) 't)
-                 (replace-match (concat ord "\\&")))))
+             (progn
+               (goto-char start)
+               (while (< (point) end)
+                 (let ((rule (car pair))
+                       (ord (cdr pair)))
+                   (when
+                       (search-forward-regexp rule end 't)
+                     (replace-match (concat ord "\\&")))
+                   (forward-line)))))
            edd-scala-sort-imports-rules)
+          (sort-lines nil start end)
+          (goto-char start)
+          (while (search-forward-regexp "^\\([0-9]\\)import\\b" nil 't)
+            (replace-match "import")))))))
 
-          (forward-line))
-        (sort-lines nil start end)
-        (goto-char start)
-        (while (search-forward-regexp "^\\([0-9]\\)import\\b")
-          (replace-match "import"))
-        ))
-      ))
-
-
+(defun edd-scala-ignore-style (start end)
+  "Ignore a scalastyle rule. If region is active it will be
+   wrapped in a scalastyle:off/scalastyle:on comment pair. If not, a
+   scalastyle:ignore comment will be used."
+  (interactive "r")
+  (let ((rule (completing-read "style-rule "
+                               '("file.size.limit" "line.size.limit" "line.contains.tab" "header.matches" "newline.at.eof" "no.newline.at.eof" "regex" "whitespace.end.of.line" "class.name" "covariant.equals" "cyclomatic.complexity" "equals.hash.code" "if.brace" "illegal.imports" "magic.number" "method.length" "method.name" "no.clone" "no.finalize" "no.whitespace.after.left.bracket" "no.whitespace.before.left.bracket" "null" "number.of.methods" "number.of.types" "object.name" "package.object.name" "parameter.number" "public.methods.have.type" "return" "simplify.boolean.expression" "spaces.after.plus" "spaces.before.plus" "structural.type" "uppercase.l" "var.field" "var.local" "while"))))
+    (save-excursion
+      (if (region-active-p)
+          (progn
+            (goto-char end)
+            (end-of-line)
+            (newline)
+            (insert "// scalastyle:on " rule)
+            (indent-according-to-mode)
+            (goto-char start)
+            (beginning-of-line)
+            (open-line 1)
+            (insert "// scalastyle:off " rule)
+            (indent-according-to-mode))
+        (progn
+          (end-of-line)
+          (insert " // scalastyle:ignore " rule))))))
 
 (add-hook 'java-mode-hook 'edd-java-hook)
 
