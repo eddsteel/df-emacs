13165402137d00a55c7ab9828846fb38cf15f0e4
Edd Steel <edward.steel@gmail.com>
2016-11-24 21:08:18 -0800

    let's try mu4e!

--- edd/edd-mail.el
+++ edd/edd-mail.el
@@ -1,7 +1,7 @@
 (defun edd-mailbox ()
   (interactive)
   "Open default mailbox"
-  (notmuch))
+  (mu4e))
 
 (setq mail-specify-envelope-from t
       mail-envelope-from 'header
@@ -37,8 +37,60 @@
   ("C-c n" . notmuch))
 
 (use-package mu4e
+  :commands mu4e
+  :load-path "/usr/share/emacs/site-lisp/mu4e"
   :init
+  (setq mu4e-use-fancy-chars 't)
   (setq mu4e-maildir "~/spool")
-  (setq mu4e-get-mail-command "offlineimap"))
+
+
+  :config
+  (edd-with-secrets "mail"
+                    (setq mu4e-contexts
+                          `( ,(make-mu4e-context
+                               :name "home"
+                               :enter-func (lambda () (mu4e-message "context: home"))
+                               :match-func
+                               (lambda (msg)
+                                 (when msg
+                                   (mu4e-message-contact-field-matches
+                                    msg :to edd-home-mail-address)))
+                               :vars `(
+                                       (mu4e-drafts-folder     . "/gmail/drafts")
+                                       (mu4e-sent-folder       . "/gmail/sent")
+                                       (mu4e-trash-folder      . "/gmail/trash")
+                                       (mu4e-refile-folder     . "/gmail/archive")
+                                       (mu4e-maildir-shortcuts .
+                                                               (("/gmail/INBOX"   . ?i)
+                                                                ("/gmail/sent"    . ?s)
+                                                                ("/gmail/trash"   . ?t)
+                                                                ("/gmail/archive" . ?a)))
+                                       (user-mail-address . ,edd-home-mail-address)))
+                             ,(make-mu4e-context
+                               :name "work"
+                               :enter-func (lambda () (mu4e-message "context: work"))
+                               :match-func
+                               (lambda (msg)
+                                 (when msg
+                                   (mu4e-message-contact-field-matches
+                                    msg :to edd-work-mail-address)))
+                               :vars `(
+                                       (mu4e-drafts-folder     . "/work/drafts")
+                                       (mu4e-sent-folder       . "/work/sent")
+                                       (mu4e-trash-folder      . "/work/trash")
+                                       (mu4e-refile-folder     . "/work/archive")
+                                       (mu4e-maildir-shortcuts .
+                                                               (("/work/INBOX"   . ?i)
+                                                                ("/work/sent"    . ?s)
+                                                                ("/work/trash"   . ?t)
+                                                                ("/work/archive" . ?a)))
+
+                                       (user-mail-address . ,edd-work-mail-address)
+                                       (mu4e-compose-signature . ,edd-work-mail-signature))))))
+  (setq mu4e-get-mail-command "offlineimap -o")
+  (setq mu4e-context-policy 'pick-first)
+  (setq mu4e-sent-messages-behavior 'delete)
+  :bind
+  ("C-c m" . mu4e))
 
 (provide 'edd-mail)
