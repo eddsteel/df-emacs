e777a05fcdd678e3f3681f7f14fd834bc649ef5c
Edd Steel <edward.steel@hootsuite.com>
2016-12-15 13:54:06 -0800

    really helpful move file command

--- edd/edd-util.el
+++ edd/edd-util.el
@@ -123,4 +123,24 @@ From a program takes two point or marker arguments, BEG and END."
       (when fix-newline
         (delete-char -1)))))
 
+;; From http://zck.me/emacs-move-file
+;;
+(defun edd-move-file (new-location)
+  "Write this file to NEW-LOCATION, and delete the old one."
+  (interactive (list (if buffer-file-name
+                         (read-file-name "Move file to: ")
+                       (read-file-name "Move file to: "
+                                       default-directory
+                                       (expand-file-name (file-name-nondirectory (buffer-name))
+                                                         default-directory)))))
+  (when (file-exists-p new-location)
+    (delete-file new-location))
+  (let ((old-location (buffer-file-name)))
+    (write-file new-location t)
+    (when (and old-location
+               (file-exists-p new-location)
+               (not (string-equal old-location new-location)))
+      (delete-file old-location))))
+
+
 (provide 'edd-util)
