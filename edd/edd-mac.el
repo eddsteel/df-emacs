42c1803645472633988193578ac8d25e29079a8e
Edd Steel <edward.steel@hootsuite.com>
2016-12-15 14:10:59 -0800

    font wibble

--- edd/edd-mac.el
+++ edd/edd-mac.el
@@ -62,4 +62,11 @@
                        (match-string 1))))))
     (setq emms-volume-change-function 'edd-emms-volume-m-change))
 
+(defun edd-wibble-font ()
+  "Switch GUI font between different sizes (switching between laptop and monitor)"
+  (interactive)
+  (if (eq (face-attribute 'default :height) 120)
+      (set-face-attribute 'default (selected-frame) :height 140)
+    (set-face-attribute 'default (selected-frame) :height 120)))
+
 (provide 'edd-mac)
