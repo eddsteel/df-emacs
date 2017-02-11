5955e92c8973d93af26e194e1af35d97b2ae52b7
Edd Steel <edward.steel@hootsuite.com>
2017-02-10 17:54:22 -0800

    colorize compilation buffers

--- edd/edd-features.el
+++ edd/edd-features.el
@@ -149,4 +149,13 @@
 ;; keep system clipboard in kill ring when overwriting it
 (setq save-interprogram-paste-before-kill t)
 
+;; colorize compilation buffers
+;; From http://stackoverflow.com/questions/13397737/ansi-coloring-in-compilation-mode
+(ignore-errors
+  (require 'ansi-color)
+  (defun my-colorize-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+  (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
+
 (provide 'edd-features)
