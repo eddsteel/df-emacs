1fdb5181da275c888c96d1ff858ffa793436a7ee
Edd Steel <edward.steel@hootsuite.com>
2017-07-04 14:45:44 -0700

    Make protobuf more auto-indenty

--- init.el
+++ init.el
@@ -470,7 +470,14 @@
 
 (use-package cider)
 
-(use-package protobuf-mode)
+(use-package protobuf-mode
+  :init
+  (defconst my-protobuf-style
+    '((c-basic-offset . 2)
+       (indent-tabs-mode . nil)))
+
+   (add-hook 'protobuf-mode-hook
+     (lambda () (c-add-style "my-style" my-protobuf-style t))))
 
 (edd/maybe-load-config "local.el")
 ;; acknowledgements
