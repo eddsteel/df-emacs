1566c387603bac2f7cc6dc4cb319c4088ec9543b
Edd Steel <edward.steel@gmail.com>
2018-06-03 15:47:21 -0700

    Fix git web link for master

--- edd/edd-git-web-link.el
+++ edd/edd-git-web-link.el
@@ -5,9 +5,10 @@
 
 (defun edd-git-web-link-capture (&rest args)
   ""
-  (shell-command (apply #'concat (-interpose " " (cons "git web-link" args))))
-  (substring
-   (shell-command-to-string (apply #'concat (-interpose " " (cons "git web-link" args)))) 0 -1))
+  (let
+      ((cmd (apply #'concat (-interpose " " (cons "git web-link" args)))) )
+    (substring
+     (shell-command-to-string cmd) 0 -1)))
 
 (defun edd-git-web-link-remote ()
   "Work out the current remote (uses magit, falls back to 'origin')"
@@ -17,56 +18,65 @@
 
 (defun edd-git-web-link-current-file (&rest args)
   "Derive link for current file in the web provider."
-  (interactive)
-  (edd-git-web-link-capture (concat (edd-git-web-link-remote) args)
-                            (file-relative-name (buffer-file-name))))
+  (apply 'edd-git-web-link-capture
+         (append
+          args
+          (list
+           (edd-git-web-link-remote)
+           (file-relative-name (buffer-file-name))))))
 
 (defun edd-git-web-link-browse-current-file ()
   "Open current file in the web provider on master."
   (interactive)
-  (browse-url (call-interactively #'edd-git-web-link-current-file)))
+  (browse-url (edd-git-web-link-current-file)))
 
 (defun edd-git-web-link-browse-current-file-master ()
   "Open current file in the web provider on master."
   (interactive)
-  (browse-url (call-interactively #'edd-git-web-link-current-file '("-b master"))))
+  (browse-url (edd-git-web-link-current-file "-b" "master")))
 
 
-(defun edd-git-web-link-current-line ()
+(defun edd-git-web-link-current-line (&rest args)
   "Derive link for current line in the web provider."
-  (interactive)
-  (edd-git-web-link-capture (edd-git-web-link-remote)
-                            (file-relative-name (buffer-file-name))
-                            (int-to-string (line-number-at-pos))))
+  (apply 'edd-git-web-link-capture
+         (append
+          args
+          (list
+           (edd-git-web-link-remote)
+           (file-relative-name (buffer-file-name))
+           (int-to-string (line-number-at-pos))))))
 
 (defun edd-git-web-link-browse-current-line ()
   "Open current line in the web provider."
   (interactive)
-  (browse-url (call-interactively #'edd-git-web-link-current-line)))
+  (browse-url (edd-git-web-link-current-line)))
 
 (defun edd-git-web-link-browse-current-line-master ()
   "Open current line in the web provider on master."
   (interactive)
-  (browse-url (call-interactively #'edd-git-web-link-current-line '("-b master"))))
+  (browse-url (edd-git-web-link-current-line "-b" "master")))
 
 
-(defun edd-git-web-link-current-region ()
+(defun edd-git-web-link-current-region (&rest args)
   "Derive link for current region in the web provider."
-  (interactive)
-  (edd-git-web-link-capture (edd-git-web-link-remote)
-                            (file-relative-name (buffer-file-name))
-                            (int-to-string (line-number-at-pos (region-beginning)))
-                            (int-to-string (line-number-at-pos (region-end)))))
+  (apply 'edd-git-web-link-capture
+         (append
+          args
+          (list
+           (edd-git-web-link-remote)
+           (file-relative-name (buffer-file-name))
+           (int-to-string (line-number-at-pos (region-beginning)))
+           (int-to-string (line-number-at-pos (region-end)))))))
 
 (defun edd-git-web-link-browse-current-region ()
   "Open current region in the web provider."
   (interactive)
-  (browse-url (call-interactively #'edd-git-web-link-current-region)))
+  (browse-url (edd-git-web-link-current-region)))
 
 (defun edd-git-web-link-browse-current-region-master ()
   "Open current region in the web provider."
   (interactive)
-  (browse-url (call-interactively #'edd-git-web-link-current-region '("-b master"))))
+  (browse-url (edd-git-web-link-current-region "-b" "master")))
 
 ;; TODO: how to jump out to s
 (defhydra hydra-edd-git-web-link (:exit nil :columns 3)
