8152b17d68b6f12f9a2e0e80043033db07a72251
Edd Steel <edward@treasure-data.com>
2018-08-10 11:07:43 -0700

    Add individual secret getter and fix gpg

--- edd/edd-secrets.el
+++ edd/edd-secrets.el
@@ -9,4 +9,11 @@
                 ,body)
          (message (format "Couldn't load %s in secrets dir" ,stub)))))
 
+(defun edd-secrets-get (name)
+  "Get an individual secret from keyring or other external command"
+  (when (eq 'darwin system-type)
+      (letrec
+          ((command (string-join `("security find-generic-password -a" ,user-login-name "-s" ,name "-w") " ")))
+        (substring (shell-command-to-string command) 0 -1))))
+
 (provide 'edd-secrets)
