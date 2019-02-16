ec878b24a2894e1c22f6346a49d1728f360e81f9
Edd Steel <edd@eddsteel.com>
2019-02-15 16:35:59 -0800

    Update mail for fastmail

--- edd/edd-mail.el
+++ edd/edd-mail.el
@@ -14,12 +14,12 @@
     "Open default mailbox"
     (mu4e))
   (setq mu4e-use-fancy-chars t)
-  (setq mu4e-maildir "~/spool")
+  (setq mu4e-maildir (expand-file-name "~/var/spool"))
   (setq mu4e-hide-index-messages t)
   (setq mu4e-headers-skip-duplicates t)
   (setq mu4e-change-filenames-when-moving t)
   (setq smtpmail-queue-mail nil
-        stmpmail-queue-dir "~/spool/queue/cur/")
+        stmpmail-queue-dir (expand-file-name "~/var/spool/queue/cur/"))
   (setq mail-user-agent 'mu4e-user-agent)
   (add-hook 'message-mode-hook
             '(lambda ()
@@ -28,7 +28,7 @@
   :config
   (edd-with-secrets "mail"
                     (setq mu4e-contexts
-                          `( ,(make-mu4e-context
+                          `(,(make-mu4e-context
                                :name "home"
                                :enter-func (lambda () (mu4e-message "context: home"))
                                :match-func
@@ -37,42 +37,23 @@
                                    (mu4e-message-contact-field-matches
                                     msg :to edd-home-mail-address)))
                                :vars `(
-                                       (mu4e-drafts-folder     . "/gmail/drafts")
-                                       (mu4e-sent-folder       . "/gmail/sent")
-                                       (mu4e-trash-folder      . "/gmail/trash")
-                                       (mu4e-refile-folder     . "/gmail/archive")
+                                       (mu4e-drafts-folder     . "/home/drafts")
+                                       (mu4e-sent-folder       . "/home/sent")
+                                       (mu4e-trash-folder      . "/home/trash")
+                                       (mu4e-refile-folder     . "/home/Archive")
                                        (mu4e-maildir-shortcuts .
-                                                               (("/gmail/INBOX"   . ?i)
-                                                                ("/gmail/sent"    . ?s)
-                                                                ("/gmail/trash"   . ?t)
-                                                                ("/gmail/archive" . ?a)))
+                                                               (("/home/INBOX"   . ?i)
+                                                                ("/home/sent"    . ?s)
+                                                                ("/home/trash"   . ?t)
+                                                                ("/home/Archive" . ?a)))
                                        (user-mail-address . ,edd-home-mail-address)
-                                       (mu4e-compose-signature . "")))
-                             ,(make-mu4e-context
-                               :name "work"
-                               :enter-func (lambda () (mu4e-message "context: work"))
-                               :match-func
-                               (lambda (msg)
-                                 (when msg
-                                   (mu4e-message-contact-field-matches
-                                    msg :to edd-work-mail-address)))
-                               :vars `(
-                                       (mu4e-drafts-folder     . "/work/drafts")
-                                       (mu4e-sent-folder       . "/work/sent")
-                                       (mu4e-trash-folder      . "/work/trash")
-                                       (mu4e-refile-folder     . "/work/archive")
-                                       (mu4e-maildir-shortcuts .
-                                                               (("/work/INBOX"   . ?i)
-                                                                ("/work/sent"    . ?s)
-                                                                ("/work/trash"   . ?t)
-                                                                ("/work/archive" . ?a)))
-
-                                       (user-mail-address . ,edd-work-mail-address)
-                                       (mu4e-compose-signature . ,edd-work-mail-signature))))))
-  (setq mu4e-get-mail-command "~/bin/syncmail")
+                                       (mu4e-compose-signature . ""))))))
+  (setq mu4e-get-mail-command "mbsync -a")
   (setq mu4e-context-policy 'pick-first)
   (setq mu4e-compose-context-policy 'ask-if-none)
-  (setq mu4e-sent-messages-behavior 'delete)
+  (setq mu4e-sent-messages-behavior 'sent)
+  (setq mu4e-change-filenames-when-moving t)
+
   :bind
   ("C-c m" . mu4e)
   ("C-c n" . edd-mailbox))
